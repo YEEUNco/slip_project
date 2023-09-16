042a6bc73c43d42177baf8371d6c6a3a98683019
yeeun <pariscoco1425@gmail.com>
2023-09-16 10:30:18 +0900

    update

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -113,7 +113,7 @@ public class WordCRUD implements ICRUD{
         String meaning = s.nextLine();
         Word word = list.get(idlist.get(id-1));
         word.setMeaning(meaning);
-        //System.out.println("단어가 수정되었습니다. ");
+        System.out.println("단어가 수정되었습니다. ");
     }
 
     public void deleteItem(){
